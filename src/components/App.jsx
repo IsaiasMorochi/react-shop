301ec0c38d3fea95ff6fc4ac35c65612ff216778
IsaiasMorochi <imorochi.dev@gmail.com>
2021-12-26 12:21:04 -0400

    Cambios en tiemo real, intalacion de css y sass loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import '../styles/global.scss';
 
 const App = () => {
   return (
-    <div>
       <h1>Hello World</h1>
-    </div>
   ); 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path'); //path del proyecto principal
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //traemos el plugin
 //de html
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //traemos el plugin de css
+
 
 module.exports = {
     entry: './src/index.js', // punto de entrada
@@ -8,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'), //en nuestro path, crea la carpeta dist
         filename: 'bundle.js' // nombre del archivo js resultante
     },
+    mode: 'development',
     resolve: { // extensión de archivos a tomar en cuenta
         extensions: ['.js', '.jsx']
     },
@@ -27,13 +30,30 @@ module.exports = {
                         loader: 'html-loader' // loader para usar
                     }
                 ]
-            }
+            },
+            {   test: /\.s[ac]ss$/i, // extensiones css
+                use: [ // usamos los loaders
+                    "style-loader", // para usar el css en el html
+                    "css-loader", // para usar el css en el js
+                    "sass-loader", // para usar el sass en el css
+                ],
+            },
         ]
     },
     plugins: [ // plugins 
         new HtmlWebpackPlugin({ // instanciamos el plugin para html 
             template: './public/index.html', // archivo raíz a transformar
             filename: './index.html' // el archivo resultante
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({ // instanciamos el plugin para css
+            filename: '[name].css' // nombre del archivo resultante
+        }),
+    ],
+    devServer: { // configuración del servidor
+        static: {
+            directory: path.resolve(__dirname, 'dist'), // carpeta donde están los archivos
+        },
+        compress: true, // comprimir
+        port: 3005, // puerto
+    }
 }
